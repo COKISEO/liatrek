50f5902639fa1fb87ce6c3f3a2db7a5de796e441
bignewshook <hannasinga01@gmail.com>
2025-03-02 03:37:25 +0700

    Add files via upload

---
+++ src/config/site.ts
@@ -0,0 +1,12 @@
+export type SiteConfig = typeof siteConfig;
+
+export const siteConfig = {
+  name: "LiatREK - Cek Nama Rekening Bank/e-Wallet Indonesia",
+  description: "Cek Nama Rekening Bank/e-Wallet Indonesia",
+  navItems: [],
+  navMenuItems: [],
+  links: {
+    github: "https://github.com/cokiseo/liatrekening.github.io",
+    docs: "https://documenter.getpostman.com/view/comingsooncokiseo",
+  },
+};
